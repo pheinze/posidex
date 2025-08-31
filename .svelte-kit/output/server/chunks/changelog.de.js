b65fbabd89c386e4c64be0fedb036ed3bef55a12
pheinze <pheinze82@gmail.com>
2025-08-31 22:02:10 +0200

    Projekt bereinigen und .gitignore hinzufügen

--- .svelte-kit/output/server/chunks/changelog.de.js
+++
@@ -1,4 +0,0 @@
-const changelog_de = '*feedback@posidex.app*\n\n**support&thanks**: bc1qgrm2kvs27rfkpwtgp5u7w0rlzkgwrxqtls2q4f\n\ntrade with Bitunix => https://www.bitunix.com/register?inviteCode=cw291y\n\n***\n\n# Changelog\n\n## Version 0.92b (22. August 2025)\n- **Verbesserung:** Eingabefeld für Symbol akzeptiert jetzt Buchstaben und Zahlen.\n- **Behoben:** Rand der Tooltips ist jetzt themenabhängig und das Problem des doppelten Randes wurde behoben.\n- **Verbesserung:** Buttons "Trade zum Journal hinzufügen" und "Anleitung anzeigen" sind jetzt themenabhängig.';
-export {
-  changelog_de as default
-};
--- .svelte-kit/output/server/chunks/changelog.en.js
+++
@@ -1,4 +0,0 @@
-const changelog_en = '*feedback@posidex.app*\n\n**support&thanks**: bc1qgrm2kvs27rfkpwtgp5u7w0rlzkgwrxqtls2q4f\n\ntrade with Bitunix => https://www.bitunix.com/register?inviteCode=cw291y\n***\n# Changelog\n\n## Version 0.92b (August 22, 2025)\n- **Improvement:** Symbol input field now accepts letters and numbers.\n- **Fixed:** Tooltip border is now theme-aware and the double border issue has been resolved.\n- **Improvement:** "Add Trade to Journal" and "Show Instructions" buttons are now theme-aware.';
-export {
-  changelog_en as default
-};
